2b859dd1eecc3a8991a8e3a111555c5528e0b3eb
Charlotte Delvaux <charlotte.delvaux@vauxdell.be>
2022-04-16 18:44:12 +0200

    Fix failing test for seeder module

--- apps/gaandeweg-server/src/app/seeder/seeder.controller.spec.ts
+++ apps/gaandeweg-server/src/app/seeder/seeder.controller.spec.ts
@@ -1,13 +1,40 @@
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
 import { Test, TestingModule } from '@nestjs/testing';
 import { SeederController } from './seeder.controller';
+import { SeederService } from './seeder.service';
 
+const moduleMocker = new ModuleMocker(global);
+
+/**
+ * Tests the SeederController class.
+ * @returns None
+ */
 describe('SeederController', () => {
   let controller: SeederController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SeederController],
-    }).compile();
+    })
+      .useMocker((token) => {
+        if (token === SeederService) {
+          return {
+            startSeed: () =>
+              jest.fn().mockResolvedValue({
+                message: 'Success!',
+              }),
+          };
+        }
+        if (typeof token === 'function') {
+          const mockMetadata = moduleMocker.getMetadata(
+            token
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          ) as MockFunctionMetadata<any, any>;
+          const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+          return new Mock();
+        }
+      })
+      .compile();
 
     controller = module.get<SeederController>(SeederController);
   });
