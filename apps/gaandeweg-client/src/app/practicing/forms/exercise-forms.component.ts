99facac67e32a570a1e1bde02a10139c40e45373
charlotte.delvaux <charlotte.delvaux@intracto.com>
2022-05-12 14:34:44 +0200

    Add formbuilder

--- apps/gaandeweg-client/src/app/practicing/forms/exercise-forms.component.ts
+++ apps/gaandeweg-client/src/app/practicing/forms/exercise-forms.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Exercise } from '../models/exercise.model';
 import { ExercisesService } from '../services/exercises.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'gaandeweg-ws-exercise-forms',
@@ -10,9 +11,26 @@ import { ExercisesService } from '../services/exercises.service';
 })
 export class ExerciseFormsComponent implements OnInit {
   exerciseForm: Observable<Exercise> | undefined;
+
+  exerciseFormGroup = new FormGroup({
+    date: new FormControl('', [Validators.required]),
+    time: new FormControl('', [Validators.required]),
+    suicidalThoughts: new FormControl('', [Validators.required]),
+    selfHarm: new FormControl('', [Validators.required]),
+    alcohol: new FormControl('', [Validators.required]),
+  });
+
   constructor(private exercisesService: ExercisesService) {}
 
   ngOnInit() {
     this.exerciseForm = this.exercisesService.getExercise(1);
+  }
+
+  onSubmit() {
+    console.log(this.exerciseFormGroup.value);
+  }
+
+  submitForm() {
+    console.log(this.exerciseFormGroup.value);
   }
 }
--- apps/gaandeweg-client/src/app/practicing/practicing.page.ts
+++ apps/gaandeweg-client/src/app/practicing/practicing.page.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ExercisesService } from './services/exercises.service';
 import { Exercise } from './models/exercise.model';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'gaandeweg-ws-practicing',
@@ -11,14 +17,45 @@ import { Exercise } from './models/exercise.model';
 export class PracticingPage implements OnInit {
   exercises: Observable<Exercise[]> | undefined;
   activeExercise: Observable<Exercise> | undefined;
-  constructor(private exercisesService: ExercisesService) {}
+
+  ionicForm!: FormGroup;
+  isSubmitted = false;
+
+  constructor(
+    private exercisesService: ExercisesService,
+    public formBuilder: FormBuilder
+  ) {}
 
   ngOnInit() {
     this.exercises = this.exercisesService.getExercises();
+
+    this.ionicForm = this.formBuilder.group({
+      date: ['', Validators.required],
+      time: ['', Validators.required],
+      suicidalThoughts: ['', Validators.required],
+      selfHarm: ['', Validators.required],
+      alcohol: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    console.log(this.ionicForm.value);
+    this.isSubmitted = true;
+    if (!this.ionicForm.valid) {
+      console.log('Please provide all the required values!');
+      return false;
+    } else {
+      console.log(this.ionicForm.value);
+      return true;
+    }
   }
 
   setActive(id: number) {
     this.activeExercise = this.exercisesService.getExercise(id);
     console.log('active exercise: ', this.activeExercise);
+  }
+
+  get errorControl() {
+    return this.ionicForm.controls;
   }
 }
