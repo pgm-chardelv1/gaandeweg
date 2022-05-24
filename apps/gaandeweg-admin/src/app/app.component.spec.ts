84dc02fc91e3210cea99156f344856ca6108f121
Charlotte Delvaux <charlotte.delvaux@vauxdell.be>
2022-05-24 20:14:02 +0200

    Fix CI/CD failing on tests

--- apps/gaandeweg-admin/src/app/app.component.spec.ts
+++ apps/gaandeweg-admin/src/app/app.component.spec.ts
@@ -1,13 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent, NxWelcomeComponent],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -17,18 +16,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'gaandweg-admin'`, () => {
+  it(`should have as title 'gaandeweg-admin'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('gaandweg-admin');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome gaandweg-admin'
-    );
+    expect(app.title).toEqual('gaandeweg-admin');
   });
 });
--- apps/gaandeweg-admin/src/main.ts
+++ apps/gaandeweg-admin/src/main.ts
@@ -1,13 +1,17 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, TRANSLATIONS } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+const translations = './locale/messages.nl.xlf';
 
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic()
-  .bootstrapModule(AppModule)
+  .bootstrapModule(AppModule, {
+    providers: [{ provide: TRANSLATIONS, useValue: translations }],
+  })
   .catch((err) => console.error(err));
