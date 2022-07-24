1874d2f41d2c8805105d52a6f6035f806eca6eb4
Charlotte Delvaux <charlotte.delvaux@vauxdell.be>
2022-07-25 00:45:36 +0200

    Update CORS policy server

--- apps/gaandeweg-server/src/main.ts
+++ apps/gaandeweg-server/src/main.ts
@@ -12,7 +12,12 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: ['http://localhost:4200', 'http://localhost:4201'],
+      origin: [
+        'http://localhost:4200',
+        'http://localhost:4201',
+        'https://gaandeweg.onrender.com',
+        'https://gaandeweg-admin.onrender.com',
+      ],
       credentials: false,
       allowedHeaders: [
         'Content-Type',
